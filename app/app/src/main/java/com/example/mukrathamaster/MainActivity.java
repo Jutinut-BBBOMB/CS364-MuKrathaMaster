63dc4e6c41e15e072905417b74cc70f70f789c61
Jutinut Ratanamongkonkul-6609650228 <kirikaya17@gmail.com>
2025-11-19 14:10:00 +0700

    Remove test logic from Main

--- app/app/src/main/java/com/example/mukrathamaster/MainActivity.java
+++ app/app/src/main/java/com/example/mukrathamaster/MainActivity.java
@@ -16,24 +16,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // --- Test Logic ---
-
-        // 1. ลองสร้างเนื้อวัวมา 1 ชิ้น
-        Beef myBeef = new Beef();
-        Log.d("GrillTest", "เริ่มปิ้งเนื้อวัว: " + myBeef.getState()); // ควรเป็น RAW
-
-        // 2. ผ่านไป 12 วินาที (ควรเป็น RARE)
-        myBeef.updateCooking(12);
-        Log.d("GrillTest", "ผ่านไป 12 วิ: " + myBeef.getState() + " | คะแนน: " + myBeef.getScore());
-
-        // 3. ปิ้งต่ออีก 10 วินาที (รวมเป็น 22 วิ -> ควรเป็น MEDIUM_RARE)
-        myBeef.updateCooking(10);
-        Log.d("GrillTest", "ปิ้งต่ออีก 10 วิ (รวม 22): " + myBeef.getState() + " | คะแนน: " + myBeef.getScore());
-
-        // 4. ปิ้งยาวๆ จนไหม้ (รวมเป็น 42 วิ -> ควรเป็น BURNT)
-        myBeef.updateCooking(20);
-        Log.d("GrillTest", "ปิ้งต่ออีก 20 วิ (รวม 42): " + myBeef.getState() + " | คะแนน: " + myBeef.getScore());
-
-        // --- จบการทดสอบ ---
     }
 }
