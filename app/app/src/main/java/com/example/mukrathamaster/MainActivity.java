93e970a13e9c96e9ed654f46ceb64b03438495d1
Jutinut Ratanamongkonkul-6609650228 <kirikaya17@gmail.com>
2025-11-19 14:03:56 +0700

    Add logic

--- app/app/src/main/java/com/example/mukrathamaster/MainActivity.java
+++ app/app/src/main/java/com/example/mukrathamaster/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mukrathamaster;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +14,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // --- Test Logic ---
+
+        // 1. ลองสร้างเนื้อวัวมา 1 ชิ้น
+        Beef myBeef = new Beef();
+        Log.d("GrillTest", "เริ่มปิ้งเนื้อวัว: " + myBeef.getState()); // ควรเป็น RAW
+
+        // 2. ผ่านไป 12 วินาที (ควรเป็น RARE)
+        myBeef.updateCooking(12);
+        Log.d("GrillTest", "ผ่านไป 12 วิ: " + myBeef.getState() + " | คะแนน: " + myBeef.getScore());
+
+        // 3. ปิ้งต่ออีก 10 วินาที (รวมเป็น 22 วิ -> ควรเป็น MEDIUM_RARE)
+        myBeef.updateCooking(10);
+        Log.d("GrillTest", "ปิ้งต่ออีก 10 วิ (รวม 22): " + myBeef.getState() + " | คะแนน: " + myBeef.getScore());
+
+        // 4. ปิ้งยาวๆ จนไหม้ (รวมเป็น 42 วิ -> ควรเป็น BURNT)
+        myBeef.updateCooking(20);
+        Log.d("GrillTest", "ปิ้งต่ออีก 20 วิ (รวม 42): " + myBeef.getState() + " | คะแนน: " + myBeef.getScore());
+
+        // --- จบการทดสอบ ---
     }
 }
